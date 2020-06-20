23bb25d804988ccdf57b1b9d4e06d001ed62f76b
thaivan <giacatluong20121996@gmail.com>
2020-06-20 13:55:19 +0700

    [UDPATE + TEST] workMapper for pagination

--- src/main/java/gc/garcol/nalsolution/mapper/WorkMapper.java
+++ src/main/java/gc/garcol/nalsolution/mapper/WorkMapper.java
@@ -5,8 +5,12 @@ import gc.garcol.nalsolution.payload.req.RequestWork;
 import gc.garcol.nalsolution.payload.res.ResponseWork;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 
 import java.sql.Timestamp;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * @author thai-van
@@ -20,8 +24,17 @@ public interface WorkMapper {
 
     ResponseWork map(Work work);
 
+    List<ResponseWork> map(List<Work> works);
+
+    default Page<ResponseWork> map(Page<Work> works) {
+
+        List<ResponseWork> res = map(works.toList());
+        return new PageImpl<>(res, works.getPageable(), works.getTotalElements());
+
+    }
+
     default Long toMillis(Timestamp timestamp) {
-        return timestamp.getTime();
+        return Objects.nonNull(timestamp) ? timestamp.getTime() : 0L;
     }
 
     default Timestamp fromMillis(Long millis) {
--- src/test/java/gc/garcol/nalsolution/mapper/WorkMapperTest.java
+++ src/test/java/gc/garcol/nalsolution/mapper/WorkMapperTest.java
@@ -5,10 +5,15 @@ import gc.garcol.nalsolution.entity.Work;
 import gc.garcol.nalsolution.enums.WorkStatus;
 import gc.garcol.nalsolution.payload.req.RequestWork;
 import gc.garcol.nalsolution.payload.res.ResponseWork;
+import gc.garcol.nalsolution.utils.codec.JsonUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 
 import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author thai-van
@@ -46,6 +51,24 @@ public class WorkMapperTest {
         ResponseWork responseWork = WorkMapper.INSTANCE.map(work);
 
         log.info("WorkMapperTest -> mapToRes. work: {}, res: {}", work, responseWork);
+
+    }
+
+    @Test
+    public void pageRes() {
+        List<Work> works = Arrays.asList(
+                Work.builder().id(1L).name("name1").build(),
+                Work.builder().id(2L).name("name2").build(),
+                Work.builder().id(3L).name("name3").build()
+        );
+
+        List<ResponseWork> res = WorkMapper.INSTANCE.map(works);
+
+        Page<Work> page = new PageImpl(works);
+
+        Page<ResponseWork> resPage = WorkMapper.INSTANCE.map(page);
+
+        log.info("WorkMapperTest -> pageRes. {}", JsonUtil.JSON.toBeautyJson(resPage));
 
     }
 
